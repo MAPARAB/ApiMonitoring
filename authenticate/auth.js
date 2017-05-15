a29a2f5ffbd7a3816739f52b092bdbda6b1adfdc
MAPARAB <MAPARAB@users.noreply.github.com>
2017-05-15 15:26:19 +0530

    Updated the way it is build

--- authenticate/auth.js
+++ authenticate/auth.js
@@ -4,65 +4,52 @@ var PropertiesReader = require('properties-reader');
 
 var properties = PropertiesReader('/home/ApiMonitoring/properties/endpoint.properties');
 
-var auth = {
 
-		login: function(req, res)
-			{
-
-				var username = req.body.username || '';
+exports.login = function(req, res)
+				{
+					var username = req.body.username || '';
     				var password = req.body.password || '';
-
-				console.log(username + " ::::: " + username);
-
-				if (username == '' || password == '') 
-				{			
-					res.status(401);
+					
+					if (username == '' || password == '') 
+					{			
+						res.status(401);
      				 	res.json({
-        						"status": 401,
-        						"message": "Invalid credentials. Username and/or Password cannot be empty"
-     						 });
+									"status": 401,
+									"message": "Invalid credentials. Username and/or Password cannot be empty"
+								});
      					 return;
     				}
+					
+					// Fire a query to OpenAM and check if the credentials are valid
+					var endpoint = properties.get('open-am.security.endpoint');
+					var hostInfo = properties.get('olp-adapter-service-access.api.proxy.ip');
+					var portInfo = properties.get('olp-adapter-service-access.api.proxy.port');
 
-				// Fire a query to OpenAM and check if the credentials are valid
-				var endpoint = properties.get('open-am.security.endpoint');
-				var hostInfo = properties.get('olp-adapter-service-access.api.proxy.ip');
-				var portInfo = properties.get('olp-adapter-service-access.api.proxy.port');
-
-			        endpoint = endpoint.concat("?username=").concat(username).concat("&password=").concat(password);
-				console.log(endpoint);
-
+					endpoint = endpoint.concat("?username=").concat(username).concat("&password=").concat(password);
+					console.log(endpoint);		
 				
-				// An object of options to indicate where to post to
-				var post_options = {
-						        host: hostInfo,
-						 	port: portInfo,
-						        path: endpoint,
-						 	method: 'POST',
-						 	headers: {
-									'Content-Type': 'application/json',
-								  }
-						    }
-				
-			        auth.validate(post_options, res);
-		
-			     },
-
-		
-				validate: function(post_options, res) 
-				{
+					// An object of options to indicate where to post to
+					var post_options = {
+										host: hostInfo,
+										port: portInfo,
+										path: endpoint,
+										method: 'POST',
+										headers: {
+													'Content-Type': 'application/json'
+												 }
+										}
+					
 					var request = http.request(post_options, function(response)
 					{
-	                                        var body = "";
-				
+						var body = "";
+						
 						response.on('data', function(data)
 						{
 							 body += data ;
 						});
-
+						
 						response.on('end', function()
 						{
-							console.log("%^#$^#^#^%#^#^#^#^#^#");
 							console.log(body);
 							
 							var args2 = body.substring(0,body.indexOf("=")); 						
@@ -80,21 +67,19 @@ var auth = {
 							res.send(JSON.parse(result));
 						});
 					});
-
-						request.on('error', function(e)
-                                		{
-                                        		console.log('Problem with request: ' + e.message);
-							res.status(401);
-							res.json({
-		                                                        "status": 401,
-                			                                "message": "Invalid credentials"
-                                                 		});
-                               		 	});
-
-					request.end();			 
 					
+					request.on('error', function(e)
+                               {
+                               		console.log('Problem with request: ' + e.message);
+									res.status(401);
+									res.json({
+		                                       "status": 401,
+                			                    "message": "Invalid credentials"
+                                            });
+                               	});
+					
+					request.end();	
 				}
 
-	}
 
-module.exports = auth;
+
