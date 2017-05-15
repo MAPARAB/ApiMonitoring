704259611b4c298ab2a8625ce7b489f6f9b55706
root <root@localhost.localdomain>
2017-05-15 10:42:16 +0100

    security folder

---
+++ authenticate/auth.js
@@ -0,0 +1,100 @@
+var jwt = require('jwt-simple');
+var http = require('http');
+var PropertiesReader = require('properties-reader');
+
+var properties = PropertiesReader('/home/ApiMonitoring/properties/endpoint.properties');
+
+var auth = {
+
+		login: function(req, res)
+			{
+
+				var username = req.body.username || '';
+    				var password = req.body.password || '';
+
+				console.log(username + " ::::: " + username);
+
+				if (username == '' || password == '') 
+				{			
+					res.status(401);
+     				 	res.json({
+        						"status": 401,
+        						"message": "Invalid credentials. Username and/or Password cannot be empty"
+     						 });
+     					 return;
+    				}
+
+				// Fire a query to OpenAM and check if the credentials are valid
+				var endpoint = properties.get('open-am.security.endpoint');
+				var hostInfo = properties.get('olp-adapter-service-access.api.proxy.ip');
+				var portInfo = properties.get('olp-adapter-service-access.api.proxy.port');
+
+			        endpoint = endpoint.concat("?username=").concat(username).concat("&password=").concat(password);
+				console.log(endpoint);
+
+				
+				// An object of options to indicate where to post to
+				var post_options = {
+						        host: hostInfo,
+						 	port: portInfo,
+						        path: endpoint,
+						 	method: 'POST',
+						 	headers: {
+									'Content-Type': 'application/json',
+								  }
+						    }
+				
+			        auth.validate(post_options, res);
+		
+			     },
+
+		
+				validate: function(post_options, res) 
+				{
+					var request = http.request(post_options, function(response)
+					{
+	                                        var body = "";
+				
+						response.on('data', function(data)
+						{
+							 body += data ;
+						});
+
+						response.on('end', function()
+						{
+							console.log("%^#$^#^#^%#^#^#^#^#^#");
+							console.log(body);
+							
+							var args2 = body.substring(0,body.indexOf("=")); 						
+							var args1  = body.substring(body.indexOf("=")+1, body.length-1);						
+							
+							if(args2 == "token.id")
+							{
+								var result = "{\"token\" : \" "  + args1 + "\" }" ;							
+							}
+							else
+							{
+								var result = "{\"message\":\"Invalid credentials\"}";
+							}
+
+							res.send(JSON.parse(result));
+						});
+					});
+
+						request.on('error', function(e)
+                                		{
+                                        		console.log('Problem with request: ' + e.message);
+							res.status(401);
+							res.json({
+		                                                        "status": 401,
+                			                                "message": "Invalid credentials"
+                                                 		});
+                               		 	});
+
+					request.end();			 
+					
+				}
+
+	}
+
+module.exports = auth;
